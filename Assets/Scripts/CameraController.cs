c63c4fc125d6af5a146e652d7f155ba0085747fd
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-06-24 09:53:29 +0300

    Add: CameraController.script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform car;
+    public float distance;
+    public float height;
+    public float rotationDamping;
+    public float heightDamping;
+    public float zoomRatio;
+    public float defaultFOV;
+    public float rotation_vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 local_velocity = car.InverseTransformDirection(car.GetComponent<Rigidbody>().velocity);
+        if(local_velocity.z < -0.5f)
+        {
+            rotation_vector = car.eulerAngles.y + 100 ;
+        } else
+        {
+            rotation_vector = car.eulerAngles.y;
+        }
+
+        float acceleration = car.GetComponent<Rigidbody>().velocity.magnitude;
+        Camera.main.fieldOfView = defaultFOV + acceleration * zoomRatio * Time.deltaTime;
+    }
+
+    void LateUpdate()
+    {
+        float wantedAngle = rotation_vector;
+        float wantedHeight = car.position.y + height;
+        float myAngle = transform.eulerAngles.y;
+        float myHeight = transform.position.y;
+
+        myAngle = Mathf.LerpAngle(myAngle,wantedAngle,rotationDamping*Time.deltaTime);
+        myHeight = Mathf.LerpAngle(myHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        Quaternion currentRotation = Quaternion.Euler(0, myAngle, 0);
+
+        transform.position = car.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        Vector3 temp = transform.position;
+        temp.y = myHeight;
+        transform.position = temp;
+
+        transform.LookAt(car);
+
+    }
+}

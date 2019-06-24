e219ce9de3bd2bb22ad2b7b7ca813d6ff4f8112b
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-06-24 15:19:02 +0300

    spagettoo

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -8,6 +8,23 @@ public class RoadGenerator : MonoBehaviour
     [SerializeField]
     public GameObject roadStraight;
 
+    
+    [SerializeField]
+    public GameObject roadJump;
+    
+    [SerializeField]
+    public GameObject roadTurn;
+
+    Vector3 nextPosition;
+
+    Quaternion nextQuaternion;
+    Vector3 nextRotation;
+
+    public Vector3 startPosition = new Vector3(0, 04083685, 0);
+
+    public Vector3 startRotation = new Vector3(-90, 0, 0);
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +38,48 @@ public class RoadGenerator : MonoBehaviour
     }
 
     void Generate() {
-        Vector3 place = new Vector3(0, 0.04083685f ,0);
-        GameObject roadBlock = Instantiate(roadStraight, place, Quaternion.identity);
-        roadBlock.transform.Rotate(-90,0,0);
-        
+        AddStraight(startPosition, startRotation);
+        for (int i = 0; i < 10; i++) {
+            AddStraight(nextPosition, nextRotation);
+            AddJump(nextPosition, nextRotation);
+            // AddTurn(nextPosition, startRotation + new Vector3(0, -45, 0));
+            AddTurn(nextPosition, nextRotation);
+        }
+       
 
+    }
+
+    void AddStraight(Vector3 position, Vector3 rotation) {
+        GameObject roadBlock = Instantiate(roadStraight, position, Quaternion.identity);
+        roadBlock.transform.Rotate(rotation);
+        // nextPosition = position + new Vector3(0, 0, 7.11f);
+        Transform[] sp = roadBlock.GetComponentsInChildren<Transform>();
+        print(sp[2].name);
+        nextPosition = sp[2].position;
+        //nextRotation = sp[2].rotation;
+        nextRotation = sp[2].eulerAngles;
+    }
+    
+    void AddJump(Vector3 position, Vector3 rotation) {
+        GameObject roadBlock = Instantiate(roadJump, position, Quaternion.identity);
+        roadBlock.transform.Rotate(rotation);
+        Transform[] sp = roadBlock.GetComponentsInChildren<Transform>();
+        nextPosition = sp[2].position;
+        nextRotation = sp[2].eulerAngles;
+    }
+    
+    void AddTurn(Vector3 position, Vector3 rotation) {
+        GameObject roadBlock = Instantiate(roadTurn, position, Quaternion.identity);
+        roadBlock.transform.Rotate(rotation);
+        
+        Transform[] sp = roadBlock.GetComponentsInChildren<Transform>();
+        nextPosition = sp[2].position;
+        nextRotation = sp[2].eulerAngles;
+
+        // foreach(Transform tf in sp) {
+        //     print(tf.name + ", Z:" + tf.rotation.z);
+        // }
+        
+        print(sp[2].rotation.z + ", next: " + nextRotation);
     }
 }

9635e1858cd42e560f696322196fdee7255dc5e2
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-06-24 19:13:49 +0300

    refactor road generator

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -14,21 +14,13 @@ public class RoadGenerator : MonoBehaviour
 {
     public List<RoadBlock> roadBlocks;
 
-    //[SerializeField]
-    //public GameObject roadStraight;
-    //public float probabilityStraight = 50f;
-
-    //[SerializeField]
-    //public GameObject roadJump;
-    //public float probabilityJump = 10f;
-
-    //[SerializeField]
-    //public GameObject roadTurn;
-    //public float probabilityTurn = 10f;
-
     public Vector3 startPosition = new Vector3(0, 04083685, 0);
 
     public Vector3 startRotation = new Vector3(0, 0, 0);
+
+    public int roadLength = 100;
+
+    float totalProbability;
 
     Vector3 nextPosition;
 
@@ -37,6 +29,10 @@ public class RoadGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        foreach(RoadBlock rb in roadBlocks)
+        {
+            totalProbability += rb.probability;
+        }
         Generate();
     }
 
@@ -47,67 +43,46 @@ public class RoadGenerator : MonoBehaviour
     }
 
     void Generate() {
-        // AddStraight(startPosition, startRotation);
         AddBlock(roadBlocks[0], startPosition, startRotation);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < roadLength; i++) {
 
-            float rdm = Random.Range(0, 1f);
+            float rdm = Random.Range(0, totalProbability);
+            float count = 0;
+            
+            foreach (RoadBlock rb in roadBlocks)
+            {
+                count += rb.probability;
+                if (rdm <= count)
+                {
+                    if (!AddBlock(rb, nextPosition, nextRotation))
+                    {
+                        i--;
+                    }
 
-            if (rdm < 0.2f)
-            {
-                //AddJump(nextPosition, nextRotation);
-                AddBlock(roadBlocks[0], nextPosition, nextRotation);
-            }
-            else if(rdm < 0.4f)
-            {
-                //AddTurn(nextPosition, nextRotation);
-                AddBlock(roadBlocks[1], nextPosition, nextRotation);
-            } else
-            {
-                //AddStraight(nextPosition, nextRotation);
-                AddBlock(roadBlocks[2], nextPosition, nextRotation);
+                    break;
+                }
             }
         }
-       
-
     }
 
-    //void AddStraight(Vector3 position, Vector3 rotation)
-    //{
-    //    GameObject roadBlock = Instantiate(roadStraight, position, Quaternion.identity);
-    //    roadBlock.transform.Rotate(rotation);
-
-    //    Transform[] sp = roadBlock.GetComponentsInChildren<Transform>();
-    //    nextPosition = sp[2].position;
-    //    nextRotation = sp[2].eulerAngles;
-    //}
-    
-    //void AddJump(Vector3 position, Vector3 rotation)
-    //{
-    //    GameObject roadBlock = Instantiate(roadJump, position, Quaternion.identity);
-    //    roadBlock.transform.Rotate(rotation);
-
-    //    Transform[] sp = roadBlock.GetComponentsInChildren<Transform>();
-    //    nextPosition = sp[2].position;
-    //    nextRotation = sp[2].eulerAngles;
-    //}
-    
-    //void AddTurn(Vector3 position, Vector3 rotation) {
-    //    GameObject roadBlock = Instantiate(roadTurn, position, Quaternion.identity);
-    //    roadBlock.transform.Rotate(rotation);
-        
-    //    Transform[] sp = roadBlock.GetComponentsInChildren<Transform>();
-    //    nextPosition = sp[2].position;
-    //    nextRotation = sp[2].eulerAngles;
-    //}
-
-    void AddBlock(RoadBlock rb, Vector3 position, Vector3 rotation)
+    bool AddBlock(RoadBlock rb, Vector3 position, Vector3 rotation)
     {
-        GameObject roadBlock = Instantiate(rb.block, position, Quaternion.identity);
-        roadBlock.transform.Rotate(rotation);
 
-        Transform[] sp = roadBlock.GetComponentsInChildren<Transform>();
-        nextPosition = sp[2].position;
-        nextRotation = sp[2].eulerAngles;
+        GameObject newBlock = Instantiate(rb.block, position, Quaternion.identity);
+        newBlock.transform.Rotate(rotation);
+
+        Transform[] sp = newBlock.GetComponentsInChildren<Transform>();
+
+        if (sp[2].eulerAngles.y < 270 && sp[2].eulerAngles.y > 90)
+        {
+            Destroy(newBlock);
+            return false;
+        } else
+        {
+            nextPosition = sp[2].position;
+            nextRotation = sp[2].eulerAngles;
+        }
+        return true;
     }
+    
 }

1f975444a3137da3476dd94242b50a0f5d9a3bba
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-06-26 08:57:15 +0300

    mainmenu

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class UiManager : MonoBehaviour
+{
+
+    Button mainMenuButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ChangeScene(int sceneID)
+    {
+        print("CLIKCED MAINMENU");
+
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
